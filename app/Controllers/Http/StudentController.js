3207550822931301003c3d7415b1095557ba029a
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-22 13:18:30 -0300

    Criação do método create em StudentController

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,6 +1,13 @@
 'use strict'
 
+const Student = use('App/Models/Student')
+
 class StudentController {
+  async create({ request }){
+    const data = request.body
+    const user = await Student.create(data)
+    return user
+  }
 }
 
 module.exports = StudentController
