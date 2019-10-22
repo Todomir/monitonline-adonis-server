4ca0c4b471b222d10f141638203704ddfa8bab62
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-22 15:44:00 -0300

    Criação do model, controller e migration de SubjectMatter

---
+++ app/Controllers/Http/SubjectMatterController.js
@@ -0,0 +1,93 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with subjectmatters
+ */
+class SubjectMatterController {
+  /**
+   * Show a list of all subjectmatters.
+   * GET subjectmatters
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new subjectmatter.
+   * GET subjectmatters/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new subjectmatter.
+   * POST subjectmatters
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single subjectmatter.
+   * GET subjectmatters/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing subjectmatter.
+   * GET subjectmatters/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update subjectmatter details.
+   * PUT or PATCH subjectmatters/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a subjectmatter with id.
+   * DELETE subjectmatters/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = SubjectMatterController
---
+++ database/migrations/1571769567831_subject_matter_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class SubjectMatterSchema extends Schema {
+  up () {
+    this.create('subject_matters', (table) => {
+      table.increments()
+      table
+        .integer('subject_id')
+        .unsigned().notNullable()
+        .references('id')
+        .fromTable('subjects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('subject_matter_description').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('subject_matters')
+  }
+}
+
+module.exports = SubjectMatterSchema
