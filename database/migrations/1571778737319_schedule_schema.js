92b82111e599b62fbd9dc3c7211bb25b8f6df13b
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-22 18:13:56 -0300

    Criação do model, controller e migration de Schedule

---
+++ database/migrations/1571778737319_schedule_schema.js
@@ -0,0 +1,17 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class ScheduleSchema extends Schema {
+   up() {
+      this.create('schedules', table => {
+         table.increments();
+         table.timestamps();
+      });
+   }
+
+   down() {
+      this.drop('schedules');
+   }
+}
+
+module.exports = ScheduleSchema;
