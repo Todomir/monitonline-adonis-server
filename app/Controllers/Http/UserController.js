d0767377fe9e097adb2ad0cbe9c517042a0e8390
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-22 16:30:42 -0300

    Alterações no método store em UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,8 +5,19 @@ const User = use('App/Models/User')
 
 class UserController {
   async store({ request }){
-    const data = request.body
-    const user = await User.create(data)
+    const { name, cpf, email, password, course, is_tutor, subjectMatters} = request.post()
+    const user = await User.create({ name, cpf, email, password, course, is_tutor})
+
+    if (subjectMatters && subjectMatters.length > 0){
+
+      if(is_tutor != false){
+        await user.subjectMatters().attach(subjectMatters)
+        user.subjectMatters = await user.subjectMatters().fetch()
+      } else {
+        console.log('You must be a tutor to teach a subject matter.')
+      }
+      
+    }
     return user
   }
 }
