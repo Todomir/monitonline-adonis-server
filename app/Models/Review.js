e851f987d1cc07766cb4bf625667b72552c0b6ee
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-23 13:04:37 -0300

    Adição do relacionamento com User em Review

--- app/Models/Review.js
+++ app/Models/Review.js
@@ -3,6 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
 
-class Review extends Model {}
+class Review extends Model {
+  user() {
+    return this.belongsTo('App/Model/User');
+  }
+}
 
 module.exports = Review;
