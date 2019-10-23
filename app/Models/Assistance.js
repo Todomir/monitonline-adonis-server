b0db49e629ba0a0b2ce0eca15a991e919f198995
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-23 13:45:07 -0300

    Adição do relacionamento com User, SubjectMatter e Schedule em Assistance

--- app/Models/Assistance.js
+++ app/Models/Assistance.js
@@ -7,6 +7,15 @@ class Assistance extends Model {
   reviews() {
     return this.hasMany('App/Models/Review');
   }
+  student() {
+    return this.belongsTo('App/Models/User');
+  }
+  subjectMatter() {
+    return this.belongsTo('App/Models/SubjectMatter');
+  }
+  schedule() {
+    return this.belongsTo('App/Models/Schedule');
+  }
 }
 
 module.exports = Assistance;
