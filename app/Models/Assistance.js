ba82d7f1fb7e1d7ea8a290be0ec9bd20969b4c9f
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-23 13:09:07 -0300

    Adição do relacionamento com Review em Assistance

--- app/Models/Assistance.js
+++ app/Models/Assistance.js
@@ -3,6 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
 
-class Assistance extends Model {}
+class Assistance extends Model {
+  reviews() {
+    return this.hasMany('App/Models/Review');
+  }
+}
 
 module.exports = Assistance;
