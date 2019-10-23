4a20c7620dacfe25fb7578494c22a11cb122407d
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-23 10:07:16 -0300

    Criação do model, controller e migration de Review

--- database/migrations/1571771059103_user_subject_matter_schema.js
+++ database/migrations/1571771059103_user_subject_matter_schema.js
@@ -2,30 +2,30 @@
 const Schema = use('Schema');
 
 class UserSubjectMatterSchema extends Schema {
-   up() {
-      this.create('users_subject_matters', table => {
-         table
-            .integer('user_id')
-            .unsigned()
-            .index('user_id')
-            .references('id')
-            .inTable('users')
-            .onUpdate('CASCADE')
-            .onDelete('CASCADE');
-         table
-            .integer('subject_matter_id')
-            .unsigned()
-            .index('subject_matter_id')
-            .references('id')
-            .inTable('subject_matters')
-            .onUpdate('CASCADE')
-            .onDelete('CASCADE');
-      });
-   }
+  up() {
+    this.create('users_subject_matters', table => {
+      table
+        .integer('user_id')
+        .unsigned()
+        .index('user_id')
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table
+        .integer('subject_matter_id')
+        .unsigned()
+        .index('subject_matter_id')
+        .references('id')
+        .inTable('subject_matters')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    });
+  }
 
-   down() {
-      this.drop('users_subject_matters');
-   }
+  down() {
+    this.drop('users_subject_matters');
+  }
 }
 
 module.exports = UserSubjectMatterSchema;
---
+++ database/migrations/1571835885492_review_schema.js
@@ -0,0 +1,34 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class ReviewSchema extends Schema {
+  up() {
+    this.create('reviews', table => {
+      table.increments();
+      table.integer('review');
+      table
+        .integer('assistance_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('assistance')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('reviews');
+  }
+}
+
+module.exports = ReviewSchema;
