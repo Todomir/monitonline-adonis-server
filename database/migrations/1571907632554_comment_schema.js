34a7290fca97da8ce400eab7272492efd5af4cc6
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-24 06:03:36 -0300

    Criação do model, controller e migration de Comment

---
+++ database/migrations/1571907632554_comment_schema.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class CommentSchema extends Schema {
+  up() {
+    this.create('comments', table => {
+      table.increments();
+      table.string('content').notNullable();
+      table
+        .integer('assistance_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('assistances')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('comments');
+  }
+}
+
+module.exports = CommentSchema;
