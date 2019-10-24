7d06a8d373af39c46be4528dde3e17be668de062
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-24 06:05:47 -0300

    Adição do relacionamento com Assistance em Comment

--- app/Models/Comment.js
+++ app/Models/Comment.js
@@ -1,9 +1,12 @@
-'use strict'
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use('Model');
 
 class Comment extends Model {
+  assistance() {
+    return this.belongsTo('App/Models/Assistance');
+  }
 }
 
-module.exports = Comment
+module.exports = Comment;
